3c3bd026286e02b483e7621d9936dd6a2490b529
avbhardwaj <avbhardwaj@live.com>
2018-05-21 18:23:50 -0700

    Delegates and Lambda Expressions.

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegates
+{
+	public class PhotoProcessor
+	{
+		// Using general way 
+		public delegate void PhotoFilterHandler(Photo photo);
+		//public void Process(string path, PhotoFilterHandler filterHandler){}
+		// Using Action Delegate
+
+
+		public void Process(string path, Action<Photo> filterHandler)
+		{
+			//System.Action<>
+
+			//System.Func<>
+
+			var photo = Photo.Load(path);
+			var filters = new PhotoFilters();
+
+			//filters.ApplyBrightness(photo);
+			//filters.ApplyContrast(photo);
+			//filters.Resize(photo);
+
+			filterHandler(photo);
+
+			photo.Save();
+		}
+	}
+}
