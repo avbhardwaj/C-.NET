f349483ec670c5f2bb5d29cb5c7bb5c95e75542c
avbhardwaj <avbhardwaj@live.com>
2018-05-25 18:11:17 -0700

    Final Commit End OF Course

---
+++ PS2/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS2
+{
+
+	class Program
+	{
+		public static string[] FindRestaurant(string[] list1, string[] list2)
+		{
+			if (list1.Length == 0 || list2.Length == 0)
+				return null;
+
+			
+			//int x = 2;
+			
+			Dictionary<string, int> store = new Dictionary<string, int>();
+			int low = Int32.MaxValue;
+			//Stack<string>
+			List<string> res = new List<string>();
+			for (int i = 0; i < list1.Length; i++)
+			{
+				store.Add(list1[i],i);
+			}
+
+			for (int i = 0; i < list2.Length; i++)
+			{
+				if (store.ContainsKey(list2[i]))
+				{
+					store[list2[i]] += i;
+
+					if (store[list2[i]] < low)
+					{
+						res.Clear();
+						low = store[list2[i]];
+						res.Add(list2[i]);
+					}
+					else if(store[list2[i]]==low)
+					{
+						res.Add(list2[i]);
+					}
+				}
+				
+			}
+
+			return res.ToArray();
+		}
+
+
+		public static string FindLongestSubStiring(string s)
+		{
+			HashSet<char> set = new HashSet<char>();
+
+			string longestOverAll = "";
+			string longestTillNow = "";
+
+			s = s.ToLower();
+			
+			for (int i = 0; i < s.Length; i++)
+			{
+				char c = s[i];
+
+				if (set.Contains(c))
+				{
+					longestTillNow = "";
+					set.Clear();
+				}
+
+				longestTillNow += c;
+				set.Add(c);
+				if (longestTillNow.Length > longestOverAll.Length)
+					longestOverAll = longestTillNow;
+
+			}
+
+			return longestOverAll;
+		}
+
+
+
+
+
+
+
+
+
+
+
+		//string[] s = new string[];
+
+
+
+		//if(list1.Contains())
+		//			for (int i = 0; i < list1.Length; i++)
+		//			{
+		//				store.Add(list1[i],i);
+		//			}
+		//
+		//			for (int i = 0; i < list2.Length; i++)
+		//			{
+		//				if (store.ContainsKey(list2[i]))
+		//				{
+		//					store[list2[i]] += i;
+		//					
+		//					low++;
+		//
+		//				}
+		//			}
+		//
+		//			if (low == 1)
+		//				return store;
+		public static string[] FindRelativeRanks(int[] nums)
+		{
+			//int max1, max2, max3;
+			Dictionary<int,int> dict = new Dictionary<int, int>();
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				dict.Add(nums[i],i);
+			}
+
+			Array.Sort(nums);
+
+			string[] res = new string[nums.Length];
+
+			for (int i = nums.Length - 1; i >= 0; i--)
+			{
+				int k = nums.Length - 1 - i;
+				if (k == 0)
+					res[dict[nums[i]]] = "Gold Medal";
+				else if (k == 1)
+					res[dict[nums[i]]] = "Silver Medal";
+				else if (k == 2)
+					res[dict[nums[i]]] = "Bronze Medal";
+				else
+					res[dict[nums[i]]] = (k + 1).ToString();
+			}
+
+			return res;
+
+		}
+
+		public bool RotateString(string A, string B)
+		{
+
+//			char[] dupA = A.ToCharArray();
+//			char[] dupB = B.ToCharArray();
+//
+//			Array.Sort(dupA);
+//			Array.Sort(dupB);
+//
+//			string t1 = new string(dupA);
+//			string t2 = new string(dupB);
+//
+//			if (t1 == t2)
+//				return true;
+//			else
+//				return false;
+			return A.Length == B.Length && (A + A).Contains(B);
+		}
+
+		static void Main(string[] args)
+		{
+
+//			string[] s1 = new string[]{ "dsjcldesl", "Tapioca Express", "Burger King", "KFC" };
+//			string[] s2 = new string[]{ "KFC", "Shogun", "Burger King" };
+//
+//			foreach (var v in FindRestaurant(s1, s2))
+//			{
+//				Console.WriteLine(v);
+//			}
+//
+//			int x = 4;
+//			int y = 1;
+//
+//			Console.WriteLine("X : {0} and Y : {1}", Convert.ToString(x,2).PadLeft(2^31,'0'), Convert.ToString(y, 2).PadLeft(2 ^ 31, '0'));
+
+			Console.WriteLine("FindLongestSubStiring(\"Dheeraj\") is : " + FindLongestSubStiring("DhEeraj"));
+			
+			DivideByZeroException
+
+		}
+	}
+}
--- ProblemSolve/Program.cs
+++ ProblemSolve/Program.cs
@@ -1,9 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProblemSolve
 {
 	class Program
 	{
+		public static bool IsPlaindrome(String S)
+		{
+
+			char[] c = S.ToCharArray();
+			Array.Reverse(c);
+			//				S.
+			String s = new String(c);
+			if (S == s)
+				return true;
+			else
+			{
+				return false;
+			}
+		}
+		public static int NumJewelsInStones(string J, string S)
+		{
+			//if ((J.Length == 0) || (S.Length == 0))
+			//	return 0;
+			//char[] c1 = J.ToCharArray();
+			//char[] c2 = S.ToCharArray();
+
+			//Array.Sort(c1);
+			//Array.Sort(c2);
+
+			int count = 0;
+			HashSet<char> jewels = new HashSet<char>();
+			for (int i = 0; i < J.Length; i++)
+			{
+				char c = J[i];
+				jewels.Add(c);
+			}
+
+			for (int i = 0; i < S.Length; i++)
+			{
+				if (jewels.Contains(S[i]))
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
 		public static bool IsAnagram(String s1, String s2)
 		{
 			if (s1.Length != s2.Length)
@@ -18,7 +61,7 @@ namespace ProblemSolve
 			Array.Sort(c1);
 			Array.Sort(c2);
 
-			
+
 			String temp1 = new String(c1);
 			String temp2 = new String(c2);
 
@@ -31,14 +74,130 @@ namespace ProblemSolve
 			String temp3 = new String(c1);
 			String temp4 = new String(c2);
 
-			if ((temp1 == temp2) && (temp3==temp4))
+			if ((temp1 == temp2) && (temp3 == temp4))
 				return true;
 			else
 				return false;
 		}
 
+
+
+
+
+
+
+		public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+		{
+			TreeNode t3 = new TreeNode(t1.val + t2.val);
+
+			//			//Checking for Right Node and Adding
+			//			if (t1.right.val != null && t2.right.val != null)
+			//			{
+			//				t3.right.val = t1.right.val + t2.right.val;
+			//			}
+			//			else if (t1.right.val == null)
+			//			{
+			//				t3.right.val = t2.right.val;
+			//
+			//			}
+			//			else if (t2.right.val == null)
+			//			{
+			//				t3.right.val = t1.right.val;
+			//			}
+			//			//Checking for Left Node and Adding
+			//			if (t1.left.val != null && t2.left.val != null)
+			//			{
+			//				t3.left.val = t1.left.val + t2.left.val;
+			//			}
+			//			else if (t1.left.val == null)
+			//			{
+			//				t3.left.val = t2.left.val;
+			//
+			//			}
+			//			else if (t2.left.val == null)
+			//			{
+			//				t3.left.val = t1.left.val;
+			//			}
+
+			if (t1.Equals(null))
+				return t2;
+			Stack<TreeNode[]> stack = new Stack<TreeNode[]>();
+			stack.Push(new TreeNode[] { t1, t2 });
+			while (stack.Count != 0)
+			{
+				TreeNode[] t = stack.Pop();
+				if (t[0].Equals(null) || t[1].Equals(null))
+					continue;
+				t[0].val += t[1].val;
+
+				if (t[0].left.Equals(null))
+					t[0].left = t[1].left;
+				else
+				{
+					stack.Push(new TreeNode[] { t[0].left, t[1].left });
+				}
+
+				if (t[0].right.Equals(null))
+					t[0].right = t[1].right;
+				else
+				{
+					stack.Push(new TreeNode[] { t[0].right, t[1].right });
+				}
+			}
+
+			return t1;
+		}
+
+		public TreeNode BuildTree(int[] inorder, int[] postorder)
+		{
+			TreeNode t1 = new TreeNode();
+			t1.val = postorder[postorder.Length - 1];
+			//HashSet<> set = new HashSet<>();
+			for (int i = 0; i < (inorder.Length) / 2; i++)
+			{
+
+			}
+
+			return t1;
+		}
+		public string ReverseString(string s)
+		{
+
+			String revString = "";
+
+			for (int i = s.Length - 1; i <= 0; i--)
+			{
+
+				revString += s[i];
+
+			}
+
+			return revString;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
 		public static void Main(string[] args)
 		{
+			//Check Palindrome
+			bool res = IsPlaindrome("MOM");
+			Console.WriteLine(res);
+
+			//Check Jewels
+			int x = NumJewelsInStones("aA", "aAAbbbb");
+			int y = NumJewelsInStones("z", "ZZZ");
+
+			Console.WriteLine("X :" + x + "Y :" + y);
 			// the code that you want to measure comes here
 
 			Console.WriteLine("Enter 1st String : ");
@@ -56,6 +215,7 @@ namespace ProblemSolve
 			watch.Stop();
 			//var sec
 			Console.WriteLine("Prolem Computed in : " + elapsedSeconds + " seconds");
+
 
 			Console.ReadLine();
 		}
